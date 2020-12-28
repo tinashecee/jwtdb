62585210f2641035695b106162250ba97995d5a6
Tinashe <ctcheuka@gmail.com>
2020-12-28 14:32:42 +0200

    fifr comit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "",
+  HOST: "gunugu",
   DATABASE_URL: "postgres://rynyjlceuboyed:a7b562de5e1c65cc845278ec1a5cb0a16c75a2d700863d3e47a97d0d4a029510@ec2-52-20-66-171.compute-1.amazonaws.com:5432/dfbu6q4nj2uhf3",
   USER: "postgres",
-  PASSWORD: "password",
+  PASSWORD: "postgres",
   DB: "postgres",
   dialect: "postgres",
   pool: {
