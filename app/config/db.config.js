1205f3fe73831d334f74d390bf2bf83416c160bc
Tinashe <ctcheuka@gmail.com>
2020-12-28 14:25:21 +0200

    fir commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  HOST: "gunugu",
+  HOST: "",
+  DATABASE_URL: "postgres://rynyjlceuboyed:a7b562de5e1c65cc845278ec1a5cb0a16c75a2d700863d3e47a97d0d4a029510@ec2-52-20-66-171.compute-1.amazonaws.com:5432/dfbu6q4nj2uhf3",
   USER: "postgres",
   PASSWORD: "password",
   DB: "postgres",
